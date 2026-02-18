e637f42d022857bdfa2ec48dda19571d3a04b2cf
Adam Mitrani <you@example.com>
2026-02-18 07:19:48 +0100

    begin pipeline and main

---
+++ orchestration/pipeline.py
@@ -0,0 +1,8 @@
+def run_pipeline():
+    print("Running ingestion...")
+    print("Running transformation...")
+    print("Running quality checks...")
+    print("Pipeline finished successfully.")
+
+if __name__ == "__main__":
+    run_pipeline()
